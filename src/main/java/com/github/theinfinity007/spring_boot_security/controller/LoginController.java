a4d07c987974e7f0cba1483fcc6f874528a26cd6
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-08-12 00:25:21 +0530

    Configured spring to use the custom login page

---
+++ src/main/java/com/github/theinfinity007/spring_boot_security/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.github.theinfinity007.spring_boot_security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginPage(){
+        return "plain-login";
+    }
+}
--- src/main/java/com/github/theinfinity007/spring_boot_security/security/SecurityConfig.java
+++ src/main/java/com/github/theinfinity007/spring_boot_security/security/SecurityConfig.java
@@ -2,13 +2,14 @@ package com.github.theinfinity007.spring_boot_security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class DemoSecurityConfig {
+public class SecurityConfig {
 
     // Inmemory users
     @Bean
@@ -26,6 +27,22 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    // Configure spring boot security to use the custom login form
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .anyRequest().authenticated()
+        )
+                .formLogin(form ->
+                        form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/authenticate")    // No need to create controller mapping for this endpoint, spring will handle itself
+                                .permitAll()
+                );
+
+        return http.build();
     }
 
 }
