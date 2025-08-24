34c7e457f4ad382d3a22e13a40108b5a787dda7a
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-08-24 14:16:30 +0530

    Configure spring security to use the DB

--- src/main/java/com/github/theinfinity007/spring_boot_security/security/SecurityConfig.java
+++ src/main/java/com/github/theinfinity007/spring_boot_security/security/SecurityConfig.java
@@ -6,27 +6,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
-    // Inmemory users
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john = User.builder()
-                .username("john").password("{noop}test123")
-                .roles("EMPLOYEE").build();
-
-        UserDetails mary = User.builder()
-                .username("mary").password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER").build();
-
-        UserDetails susan = User.builder()
-                .username("susan").password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        // automatically uses the users and authorities table by default
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     // Configure spring boot security to use the custom login form
@@ -50,5 +42,27 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
+    /*
+    // Inmemory users
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john = User.builder()
+                .username("john").password("{noop}test123")
+                .roles("EMPLOYEE").build();
+
+        UserDetails mary = User.builder()
+                .username("mary").password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER").build();
+
+        UserDetails susan = User.builder()
+                .username("susan").password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+     */
 
 }
