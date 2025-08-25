801b7f1db2aa548d94893979653f2116f85eba12
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-08-26 01:09:14 +0530

    Update the annotation to component

--- src/main/java/com/github/theinfinity007/spring_boot_security/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/github/theinfinity007/spring_boot_security/security/CustomAuthenticationSuccessHandler.java
@@ -10,11 +10,11 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Controller
+@Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     UserService userService;
 
